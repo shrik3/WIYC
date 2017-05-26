a1d8200ea3e7bdf2dbb706adc6f909ccb8873721
EW <cshrik3@gmail.com>
2017-05-26 11:01:30 +0800

    api

--- app/Http/Controllers/Api/V1/BookController.php
+++ app/Http/Controllers/Api/V1/BookController.php
@@ -8,4 +8,12 @@ use App\Http\Controllers\Controller;
 class BookController extends Controller
 {
     //
+    public function index(){
+        $books = \App\Book::all();
+        foreach($books as $book){
+            $book['img']=url('images'.$book['img']);
+        }
+        return $books;
+
+    }
 }
