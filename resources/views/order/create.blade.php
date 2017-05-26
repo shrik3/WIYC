7341a185becfb025376ede463e1130102bc889ed
EW <cshrik3@gmail.com>
2017-05-26 12:18:23 +0800

    bug

--- resources/views/order/create.blade.php
+++ resources/views/order/create.blade.php
@@ -1,54 +1,95 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Books</title>
 
-@section('content')
+	<!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
 
+	  <link rel="stylesheet" type="text/css" href="/css/app.css">
+	  <link rel="stylesheet" type="text/css" href="/css/base.css">
+       <!-- Styles -->
 
-<style>
+</head>
+<body>
+	<?php if(Route::has('login')): ?>
+    <div class='top_bar'>
+		<div class='top-middle'>
+			<h4 align='middle'>Your Book Store</h4>
 
-</style>
-
-<div class="container">
-    <div class="row">
-        <div class="col-md-10 col-md-offset-1">
-            <div class="panel panel-default">
-                <div class="panel-heading">Logging a new book </div>
-                <div class="panel-body">
-
-                    @if (count($errors) > 0)
-                        <div class="alert alert-danger">
-                            <strong>新增失败</strong> 输入不符合要求<br><br>
-                            {!! implode('<br>', $errors->all()) !!}
-                        </div>
-                    @endif
-
-                    <form action="{{ url('admin/book') }}" method="POST" enctype="multipart/form-data">
-                        {!! csrf_field() !!}
-                        <input type="text" name="name" class="form-control" required="required" placeholder="请输入名称 (无需书名号)">
-                        <br>
-                        <input type="text" name="ISBN" class="form-control" required="required" placeholder="请输入ISBN">
-                        <br>
-                        <input type="text" name="info" class="form-control" required="required" placeholder="请输入出版信息">
-                        <br>
-                        <input type="text" name="amount" class="form-control" required="required" placeholder="请输入数量">
-                        <br>
-                        <input type="text" name="base_price" class="form-control" required="required" placeholder="基础价格">
-                        <br>
-                        <input type="text" name="sale_price" class="form-control" required="required" placeholder="售价">
-                        <br>
-                        <textarea name="brief" rows="10" class="form-control" required="required" placeholder="请输入简介"></textarea>
-                        <br>
-                        <div class="row">
-            				<div class="col-md-12">
-            					<input type="file" name="image" required="required"  />
-            				</div>
-                        </div>
-                        <br>
-                        <button class="btn btn-lg btn-info">New entry</button>
-                    </form>
-
-                </div>
-            </div>
+		</div>
+        <div class="top-right links">
+             <?php if(Auth::check()): ?>
+                <a href="<?php echo e(url('/home')); ?>">Home</a>
+             <?php else: ?>
+                <a href="<?php echo e(url('/login')); ?>">Login</a>
+                <a href="<?php echo e(url('/register')); ?>">Register</a>
+			<?php endif; ?>
         </div>
     </div>
+	<?php endif; ?>
+
+
+<div id="content-body">
+	<!-- 侧栏 -->
+	<div id="sidebar">
+			<div class="part2">
+				<a href="<?php echo e(url('book/'.$book->sISBN)); ?>">
+					<img height="150" width="150px" src="<?php echo e(url('/images/'.$book->img)); ?>"/>
+				</a>
+				<div class="info">
+					<p>Name : <?php echo e($book->name); ?></p>
+					<p>Info : <?php echo e($book->info); ?></p>
+					<br>
+					<p>price : <em class="price"><?php echo e($book->sale_price); ?></em></p>
+					<br>
+					<!-- <a href="/book" >
+						<button type="button" class="mbutton"  >BUY</button>
+					</a> -->
+				</div>
+
+			</div>
+
+	</div>
+
+	<div id="books-body">
+		<h3 > Create a new order </h3>
+		<hr>
+
+		<?php if(count($errors) > 0): ?>
+            <div class="alert alert-danger">
+                <strong>failed</strong> <br><br>
+                    <?php echo implode('<br>', $errors->all()); ?>
+
+            </div>
+        <?php endif; ?>
+
+        <form action="<?php echo e(url('my/order/new/'.$book->sISBN)); ?>" method="POST" enctype="multipart/form-data">
+        	<?php echo csrf_field(); ?>
+
+                <input type="text" name="name" class="form-control" style="font-weight:normal" required="required" placeholder="真实姓名">
+                    <br>
+                <input type="text" name="tel" class="form-control" style="font-weight:normal"  required="required" placeholder="联系电话">
+                    <br>
+                <input type="text" name="address" class="form-control" style="font-weight:normal"  required="required" placeholder="邮寄地址">
+                    <br>
+                <input type="text" name="amount" class="form-control" style="font-weight:normal"  required="required" placeholder="输入订购数量">
+                    <br>
+                <input type="text" name="note" class="form-control" style="font-weight:normal"  placeholder="备注（特殊要求等）">
+                    <br>
+
+
+				<button class="btn btn-lg btn-info">CONFIRM</button>
+        </form>
+
+
+
+	</div>
+
+
 </div>
-@endsection
+
+
+</body>
+</html>
